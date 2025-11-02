620cd21c70ff2c8ebd7b21ce9f391fbf8f5352bd
alecmakintosh <alec.mak@outlook.com>
2025-11-02 16:22:22 -0500

    thanks brendan

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>Proudly made by Group Five at the 2025 CUTA YLS in Montréal, Québec.</p>
+        <p>Proudly made by Group Five at the 2025 CUTA YLS in Montréal, Québec. A special thanks to Brendan Wilson for his support and guidance.</p>
         {/* REPLACE THIS TEXT WITH YOUR SINGLE LINE */}
       </div>
     </footer>
